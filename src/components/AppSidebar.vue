5ea2372be9b3883fc22f5dee496824924c6789dc
Shiny-Gyarados <hunter2015850711@gmail.com>
2025-07-31 00:34:56 +0800

    [Chore] init vue app with shadcn-vue

---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { Home, BadgeInfo } from "lucide-vue-next";
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { RouterLink } from "vue-router";
+
+const items = [
+    {
+        title: "Home",
+        url: "/",
+        icon: Home,
+    },
+    {
+        title: "About",
+        url: "/about",
+        icon: BadgeInfo,
+    },
+];
+</script>
+
+<template>
+    <Sidebar>
+        <SidebarHeader />
+        <SidebarContent>
+            <SidebarGroup>
+                <SidebarGroupLabel>Application</SidebarGroupLabel>
+                <SidebarGroupContent>
+                    <SidebarMenu>
+                        <SidebarMenuItem v-for="item in items" :key="item.title">
+                            <SidebarMenuButton asChild>
+                                <RouterLink :to="item.url">
+                                    <component :is="item.icon" />
+                                    <span>{{ item.title }}</span>
+                                </RouterLink>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarGroupContent>
+            </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter />
+    </Sidebar>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
+
+<template>
+    <main class="py-2 px-12">
+        <TheWelcome />
+    </main>
+</template>
